150040d9b7f07def29bf55c2487c94d43d3aa875
Eilin Seth <aksun989@gmail.com>
2024-12-28 06:14:11 +0700

    Membuat fungsi getTodos dan getTodo

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,8 +1,50 @@
-const getTodos = async () => {
+import {Response,Request} from "express"
+import TodoModel from "../models/todo"
+import {Todo} from "../types/todo"
 
+const getTodos = async (req:Request,res:Response):Promise<void> => {
+    try{
+        const data : Todo[] = await TodoModel.find()
+        res.status(200).json({
+            status:"200",
+            message:"OK",
+            data
+        })
+        
+    }catch(error){
+        console.error(`${error}`);
+        res.status(500).json({
+            status:500,
+            message:"Internal Server Error"
+        })
+    }
 }
 
-const getTodo = async () => {
+const getTodo = async (req:Request,res:Response):Promise<void> => {
+    try{
+        const {id} = req.params;
+        const todo = await TodoModel.findById(id)
+        if(!todo){
+            res.status(404).json({
+                status:404,
+                message:"Id not found"
+            })
+        }
+        
+        res.status(200).json({
+            status:200,
+            message:"OK",
+            todo
+        })
+        
+
+    }catch(error){
+        console.error(`${error}`)
+        res.status(500).json({
+            status:500,
+            message:"Internal Server Error"
+        })
+    }
     
 }
 
