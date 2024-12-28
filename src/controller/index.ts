f9a28860c1a1b7a971522d6d5bb66a1506359b96
Eilin Seth <aksun989@gmail.com>
2024-12-28 07:33:12 +0700

    Menambahkan fungsi addTodo , updateTodo , dan deleteTodo

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -48,16 +48,97 @@ const getTodo = async (req:Request,res:Response):Promise<void> => {
     
 }
 
-const addTodo = async () => {
-    
+const addTodo = async (req:Request,res:Response):Promise<void> => {
+    try{
+       const body : Pick<Todo,'title' | 'status'> = req.body
+
+       if(!body.title || !body.status){
+        res.status(401).json({
+            status : 401 ,
+            message : "data invalid , Unauthorized"
+        })
+        return
+       }
+
+       const newTodo = new TodoModel({
+        title:body.title,
+        status:body.status
+       })
+
+       await newTodo.save()
+       const todos = await TodoModel.find()
+
+       res.status(200).json({
+        status : 200,
+        message : "OK",
+        added : newTodo,
+        data:todos
+       })
+    }catch(error){
+        console.error(`${error}`)
+        res.status(500).json({
+            status : 500,
+            message : "Internal Server Error"
+        })
+    }
 }
 
-const updateTodo = async () => {
-    
+const updateTodo = async (req:Request,res:Response):Promise<void> => {
+    try{
+        const {params:{id},body} = req
+        
+        if(!body.title || !body.status || !id){
+            res.status(401).json({
+                status:401,
+                message:"Data invalid , Unauthorized"
+            })
+            return
+        }
+        const updatedTodo = new TodoModel({
+            title : body.title,
+            status :body.status
+        })
+
+        await TodoModel.findByIdAndUpdate({_id:id},body)
+        const todos = await TodoModel.find()
+        if(!updatedTodo){
+            res.status(501).json({
+                status:501,
+                message : "Update failed , Not implemented"
+            })
+        }
+        res.status(200).json({
+            status : 200 ,
+            message:"OK",
+            updatedTodos : todos
+        })
+    }catch(error){
+        console.error(`${error}`)
+        res.status(500).json({
+            status : 500,
+            message : "Internal Server Error"
+        })
+    }
 }
 
-const deleteTodo = async () => {
-    
+const deleteTodo = async (req:Request,res:Response):Promise<void> => {
+    try{
+        const {id} = req.params;
+        await TodoModel.findByIdAndDelete({_id:id})
+        const updatedTodos= await TodoModel.find()
+        
+        res.status(200).json({
+            status:200,
+            message: "Delete Success",
+            updatedTodos
+        })
+    }catch(error){
+        console.error(`${error}`)
+        res.status(500).json({
+            status:500,
+            message:"Internal Server Error"
+        })
+    }
 }
 
 export {getTodos , getTodo , addTodo , updateTodo , deleteTodo}
